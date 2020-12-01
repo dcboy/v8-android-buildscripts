73fd8d061c939b90f67b66f1f25f3bbc41ff0e97
Kudo Chien <ckchien@gmail.com>
2020-12-01 12:26:42 +0800

    Test [ci skip]

--- scripts/export_npm_env.sh
+++ scripts/export_npm_env.sh
@@ -3,8 +3,8 @@ if [[ ${CIRCLECI} ]]; then
   echo "export VERSION=${npm_package_version}" >> $BASH_ENV
   echo "export V8_VERSION=${npm_package_config_V8}" >> $BASH_ENV
 elif [[ ${GITHUB_ACTIONS} ]]; then
-  echo "export VERSION=${npm_package_version}" >> $GITHUB_ENV
-  echo "export V8_VERSION=${npm_package_config_V8}" >> $GITHUB_ENV
+  echo "VERSION=${npm_package_version}" >> $GITHUB_ENV
+  echo "V8_VERSION=${npm_package_config_V8}" >> $GITHUB_ENV
 else
   export VERSION=${npm_package_version}
   export V8_VERSION=${npm_package_config_V8}
